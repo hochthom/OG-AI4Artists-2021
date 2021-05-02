2d3001ced73cd230d4d4b032b26689650b72458d
Thomas Hoch <hoch55@gmail.com>
2021-05-02 17:28:20 +0200

    Remove plotting code

--- Lecture3_Pix2Pix_Example/pix2pix.py
+++ Lecture3_Pix2Pix_Example/pix2pix.py
@@ -208,19 +208,4 @@ def discriminator_loss(disc_real_output, disc_generated_output):
     return total_disc_loss
 
 
-def generate_images(model, test_input, tar):
-    prediction = model(test_input, training=True)
-    plt.figure(figsize=(15,15))
-
-    display_list = [test_input[0], tar[0], prediction[0]]
-    title = ['Input Image', 'Ground Truth', 'Predicted Image']
-
-    for i in range(3):
-        plt.subplot(1, 3, i + 1)
-        plt.title(title[i])
-        # getting the pixel values between [0, 1] to plot it.
-        plt.imshow(display_list[i] * 0.5 + 0.5)
-        plt.axis('off')
-    plt.show()
-
 
